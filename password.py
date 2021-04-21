077114d276dca5623678df2e23aa69bfb256298b
hakan-erdem <erdem.hakan646@gmail.com>
2021-04-21 17:50:27 +0300

    completed password.py task

---
+++ password.py
@@ -0,0 +1,10 @@
+name = "Hakan"
+password = "W@12"
+
+user_input = input().title()
+
+if user_input == name:
+    print(f"Hello, {user_input}! The password is: {password}")
+
+else:
+    print(f"Hello, {user_input}! See you later.")
