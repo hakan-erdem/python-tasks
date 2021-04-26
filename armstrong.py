c9bba4d6d3c78d898ae8607cf8087951b3482ab4
hakan-erdem <erdem.hakan646@gmail.com>
2021-04-26 13:44:30 +0300

    python task armstrong is done

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+n = input("Enter the number: ")
+
+if not n.isdigit():
+    print(f"{n} is an invalid entry. Don't use non-numeric, float, or negative values!")
+
+else:
+    result = 0
+
+    for item in n:
+        result += int(item) ** len(n)
+
+    if result == int(n):
+        print(f"{result} is an Armstrong number")
+
+    else:
+        print(f"{result} is not an Armstrong number")
